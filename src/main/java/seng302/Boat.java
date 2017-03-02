325b4c707d0587a5fe29662816d66834a10b409d
Tim Garrod <tjg73@uclive.ac.nz>
2017-03-02 17:10:27 +1300

    Removed DisplayNames class, added Race class and filled out basic Boat class functionality
    * race now has a display names and add boat method
    * can now get and set boats names

--- src/main/java/seng302/Boat.java
+++ src/main/java/seng302/Boat.java
@@ -4,4 +4,17 @@ package seng302;
  * Created by osr13 on 2/03/17.
  */
 public class Boat {
+    private String boatName;
+
+    public Boat(String name) {
+        this.boatName = name;
+    }
+
+    public String getBoatName() {
+        return boatName;
+    }
+
+    public void setBoatName(String boatName) {
+        this.boatName = boatName;
+    }
 }
---
+++ src/main/java/seng302/Race.java
@@ -0,0 +1,32 @@
+package seng302;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tjg73 on 2/03/17.
+ */
+public class Race {
+
+    private ArrayList<Boat> currentBoats;
+
+    /**
+     * Display the current Boats names that are in the race to std output
+     */
+    public void displayNames() {
+        if (currentBoats.size() > 0) {
+            for (Boat boat : currentBoats) {
+                System.out.println(boat.getBoatName());
+            }
+        }else {
+            System.out.println("No boats in race");
+        }
+    }
+
+    /**
+     * Adds a boat to the race
+     * @param newBoat Boat to add to race
+     */
+    public void addBoat(Boat newBoat){
+        this.currentBoats.add(newBoat);
+    }
+}
