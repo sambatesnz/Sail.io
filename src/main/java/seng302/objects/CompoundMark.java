ed990f61b7ef639a5b220a77440891af7e9f8e46
osr13 <osr13@uclive.ac.nz>
2017-03-17 13:50:20 +1300

    Implemented some distance calculators etc for compound marks.
    - Added CompoundMarks base stuff with Stefan.
    - Calculate the distance based on lat/long.
    - Find mid point in a Gate.
    
    #story[10] #refactor #implement

--- src/main/java/seng302/objects/CompoundMark.java
+++ src/main/java/seng302/objects/CompoundMark.java
@@ -11,6 +11,9 @@ public class CompoundMark {
     private ArrayList<Point> compoundMarks = new ArrayList<>();
     private String name;
 
+    public String getName() {
+        return name;
+    }
 
     class Point{
         double latitude;
@@ -52,6 +55,60 @@ public class CompoundMark {
             Point point = new Point(latitude, longitude);
             this.compoundMarks.add(point);
         }
+    }
+
+    private CompoundMark findAverageGate(CompoundMark gate){
+        CompoundMark outputGate = gate;
+        if (gate.compoundMarks.size() == 2) {
+            Point point1 = gate.compoundMarks.get(0);
+            Point point2 = gate.compoundMarks.get(1);
+            double meanLat = (point1.latitude + point2.latitude)/2;
+            double meanLong = (point1.longitude + point2.longitude)/2;
+            Point meanPoint = new Point(meanLat, meanLong);
+            ArrayList<Point> averagePoint = new ArrayList<>();
+            averagePoint.add(meanPoint);
+            outputGate = new CompoundMark(gate.name, averagePoint);
+        }
+        return outputGate;
+    }
+
+    private double findDistBetweenCompoundMarks(CompoundMark mark1, CompoundMark mark2) {
+        if (mark1.compoundMarks.size() == 2) {
+            mark1 = findAverageGate(mark1);
+        }
+        if (mark2.compoundMarks.size() == 2) {
+            mark2 = findAverageGate(mark2);
+        }
+        double lat1 = mark1.compoundMarks.get(0).latitude;
+        double lat2 = mark2.compoundMarks.get(0).latitude;
+        double long1 = mark1.compoundMarks.get(0).longitude;
+        double long2 = mark2.compoundMarks.get(0).longitude;
+
+        double distance = distance(lat1, long1, lat2, long2, "K");
+
+        return distance;
+    }
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") {
+            dist = dist * 1.609344;
+        } else if (unit == "N") {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
 
