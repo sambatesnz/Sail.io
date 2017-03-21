526f4cdbb9fb81d1a1ae0965603143b208a8e951
Oliver Robb <oliver.robb@outlook.com>
2017-03-22 12:09:14 +1300

    Debugging the MainCanvas.
    - added dots to traverse the entire canvas.
    - Stefan's fix seems to work, but I am not sure if it is too big now?
    
    #chore #fix #test #story[9]

--- src/main/java/seng302/controller/MainController.java
+++ src/main/java/seng302/controller/MainController.java
@@ -1,17 +1,23 @@
 package seng302.controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import seng302.App;
 import seng302.Model.RaceAnimationTimer;
+
+import java.util.ArrayList;
+import static java.util.Arrays.asList;
 
 /**
  * Created by Justin on 15-Mar-17.
@@ -38,6 +44,50 @@ public class MainController {
         mainCanvas.widthProperty().bind(primaryStage.widthProperty());
         mainCanvas.heightProperty().bind(primaryStage.heightProperty());
 
+
+        /**
+         * Tried to set the rect color here.
+         */
+//        GraphicsContext gc= mainCanvas.getGraphicsContext2D();
+//        gc.setFill(Color.AQUA);
+//        gc.fillRect(0,0, mainCanvas.getWidth(), mainCanvas.getHeight());
+//
+//        System.out.println(mainCanvas.widthProperty());
+
+
+        /**
+         * Using dots to explore the size of it all.
+         */
+        ArrayList<Color> boatColors = new ArrayList<>(asList(Color.CHOCOLATE, Color.GREEN, Color.CYAN, Color.GOLD, Color.DARKGREY, Color.PURPLE));
+
+        GraphicsContext gc= mainCanvas.getGraphicsContext2D();
+
+        new AnimationTimer() {
+            double x = 0;
+            double y = 0;
+
+            double canvasWidth = gc.getCanvas().getWidth();
+            double canvasHeight = gc.getCanvas().getHeight();
+
+            public void handle(long currentNanoTime) {
+                gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+
+                x += 1;
+                y += 2;
+
+                if (x > canvasWidth || y > canvasHeight) {
+                    System.out.printf("canvasWidth: %d, canvasHeight: %d.", x, y);
+                    x = 0;
+                    y = 0;
+                }
+                gc.setFill(boatColors.get(0));
+                gc.fillOval(x, y, 15, 15);
+                gc.setFill(boatColors.get(1));
+                gc.fillOval(y, x, 15, 15);
+
+
+            }
+        }.start();
 
     }
 
