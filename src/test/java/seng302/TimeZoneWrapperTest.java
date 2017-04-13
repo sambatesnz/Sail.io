a8deef96de0ea77ccfd0e745320d33e12d11d879
Ollie Robb <osr13@uclive.ac.nz>
2017-04-13 22:08:04 +1200

    Fixed some errors that were stopping Matt and Stefan testing things.
    - Commented out extra dependencies in the pom (don't know how they got there.
    - Added dummy test.

--- src/test/java/seng302/TimeZoneWrapperTest.java
+++ src/test/java/seng302/TimeZoneWrapperTest.java
@@ -1,12 +1,17 @@
-//package seng302;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by Stefan! on 13/04/2017.
-// */
-//public class TimeZoneWrapperTest {
-//
-//    @Test
-//
-//}
+package seng302;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by Stefan! on 13/04/2017.xdf
+ */
+public class TimeZoneWrapperTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+    }
+}
