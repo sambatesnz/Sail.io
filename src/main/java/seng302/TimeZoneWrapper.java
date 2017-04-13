f8f75664ef03921f745eedf8269cc34ca4bd5e13
Stefan Hall <sha162@uclive.ac.nz>
2017-04-13 16:00:48 +1200

    Cleaned up class and added documentation
    Signed-off-by: Stefan Hall <stefan-hall@hotmail.com>
    #story[28]

--- src/main/java/seng302/TimeZoneWrapper.java
+++ src/main/java/seng302/TimeZoneWrapper.java
@@ -1,32 +1,39 @@
 package seng302;
 
-import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
-
+/**
+ * A wrapper for the race time Zone
+ * Holds the UTC Offset and the current time in the time zone
+ */
 public class TimeZoneWrapper {
     private ZoneId raceTimeZoneId;
     private DateTimeFormatter raceTimeStringFormat;
-    private String raceTimeZone;
-
-    private TimeZone localTimeZone;
     private String utcOffset;
 
+    /**
+     * Constructor for TimeZone Wrapper Class
+     * @param raceTimeZone String of the timeZone you are setting
+     */
     public TimeZoneWrapper(String raceTimeZone) {
-        this.raceTimeZone = raceTimeZone;
         this.raceTimeZoneId = ZoneId.of(raceTimeZone);
         this.raceTimeStringFormat = DateTimeFormatter.ofPattern("hh:mm:ss");
-
-        Calendar calendar = new GregorianCalendar();
-        this.localTimeZone = calendar.getTimeZone();
         this.utcOffset = calculateOffset();
     }
 
+    public String getLocalTimeString(){
+        return getLocalTime().format(raceTimeStringFormat);
+    }
+
+    public String getRaceTimeZoneString(){
+        return this.utcOffset;
+    }
+
+    /**
+     * Calculates the offset in hours from the UTC time standard
+     * @return A String of the offset with UTC at the front : UTC-03:00
+     */
     private String calculateOffset(){
         ZonedDateTime zdt = LocalDateTime.now().atZone(this.raceTimeZoneId);
         ZoneOffset offset = zdt.getOffset();
@@ -37,12 +44,6 @@ public class TimeZoneWrapper {
         return LocalTime.now(this.raceTimeZoneId);
     }
 
-    public String getLocalTimeString(){
-        return getLocalTime().format(raceTimeStringFormat);
-    }
 
-    public String getRaceTimeZoneString(){
-        return this.utcOffset;
-    }
 
 }
---
+++ src/test/java/seng302/TimeZoneWrapperTest.java
@@ -0,0 +1,12 @@
+//package seng302;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by Stefan! on 13/04/2017.
+// */
+//public class TimeZoneWrapperTest {
+//
+//    @Test
+//
+//}
