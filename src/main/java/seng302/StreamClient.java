4d26cedc8ad2f66625538fe5932b51db3c7c222f
Tim Garrod <tjg73@uclive.ac.nz>
2017-05-04 11:59:02 +1200

    Changed streamClient so we can see what our server is generating
    #story[29]

--- src/main/java/seng302/StreamClient.java
+++ src/main/java/seng302/StreamClient.java
@@ -7,6 +7,7 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StreamClient {
     //    private StreamParser parser;
@@ -16,10 +17,10 @@ public class StreamClient {
     private String serverName;
     private int port;
     private byte[] data;
-    String output = "";
+//    String output = "";
 
     public StreamClient() {
-        data = new byte[1000];
+        data = new byte[4300];
         clientSocket = null;
         streamInput = null;
         AppConfig config = new AppConfig();
@@ -29,31 +30,42 @@ public class StreamClient {
 
     public void retrieveData() {
         int breakNo = 0;
-        while (clientSocket != null && streamInput != null && breakNo < 200) {
+        int result = 0;
+        while (clientSocket != null && streamInput != null && result != -1) {
             try {
-                int result = streamInput.read(data);
-                output += new String(data, 0, result);
+                System.out.println("Requesting Data: ");
+                result = streamInput.read(data);
+                System.out.println("Data read in.");
+//                output += new String(data, 0, result);
                 breakNo ++;
                 System.out.println(breakNo);
                 int syncPacket1 = data[0];
                 int syncPacket2 = data[1];
-//                System.out.printf("SP1: %d, SP2: %d", syncPacket1, syncPacket2);
-
-//                if (syncPacket1 == 71 && syncPacket2 == -125) {
-//                    // TODO: Pass to Parser.
-//                }
+                //System.out.printf("SP1: %d, SP2: %d", syncPacket1, syncPacket2);
+//                System.out.println(Arrays.toString(data));
+                byte[] dest = new byte[2];
+                System.arraycopy(data,13,dest,0,2);
+                short length = ByteBuffer.wrap(dest).order(ByteOrder.LITTLE_ENDIAN).getShort();
+                System.out.println(length);
+                if (syncPacket1 == 71 && syncPacket2 == -125) {
+                    System.out.println("Valid Packet.");
+                    // TODO: Pass to Parser.
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            Arrays.fill(data, (byte)0);
         }
         disconnect();
     }
 
     public void connect() {
         try {
-            String host = new URL(serverName).getHost();
+            String host = "132.181.13.120";
+//            String host = new URL(serverName).getHost();
             System.out.println("Attempting to connect...");
-            clientSocket = new Socket(host, port);
+//            clientSocket = new Socket(host, port);
+            clientSocket = new Socket(host, 9090);
             System.out.println("Connected.");
             streamInput = new DataInputStream(clientSocket.getInputStream());
         }
@@ -64,6 +76,7 @@ public class StreamClient {
 
     public void disconnect() {
         try {
+            System.out.println("Socket disconnected.");
             clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
