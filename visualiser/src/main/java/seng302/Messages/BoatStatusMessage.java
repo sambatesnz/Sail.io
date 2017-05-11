90a62cfec91f42119334e39563209aba0c1bd096
Jonathan Travaille <jtr44@uclive.ac.nz>
2017-05-11 18:10:21 +1200

    Parsing stream data - Story 30 Progress
    Made BoatStatusMessage class that decomposes the boat status message packets into variables for use elsewhere.
    
    #story[30]
    
    Author: Jonathan Travaille (jtr44)

---
+++ visualiser/src/main/java/seng302/Messages/BoatStatusMessage.java
@@ -0,0 +1,19 @@
+package seng302.Messages;
+
+
+public class BoatStatusMessage {
+    private long sourceID;
+    private int status;
+    private int legNumber;
+    private long estTimeToNextMark;
+    private long estTimeToFinish;
+
+    public BoatStatusMessage(long sourceID, int status, int legNumber,
+                             long estTimeToNextMark, long estTimeToFinish) {
+        this.sourceID = sourceID;
+        this.status = status;
+        this.legNumber = legNumber;
+        this.estTimeToNextMark = estTimeToNextMark;
+        this.estTimeToFinish = estTimeToFinish;
+    }
+}
---
+++ visualiser/src/main/java/seng302/Messages/RaceStatusMessage.java
@@ -0,0 +1,50 @@
+package seng302.Messages;
+
+
+public class RaceStatusMessage {
+    private long currentTime;
+    private long raceID;
+    private int raceStatus;
+    private long expectedStartTime;
+    private double windDirection;
+    private int windSpeed;
+    private int numBoatsInRace;
+    private int raceType;
+
+    private long boatSourceID;
+    private int boatStatus;
+    private int boatLegNumber;
+    private long boatTimeToNextMark;
+    private long boatTimeToFinish;
+
+    private BoatStatusMessage[] boatDetailsList;
+
+    public RaceStatusMessage(byte[] bytes) {
+        currentTime = Message.byteArrayToLong(bytes, 1, 6);
+        raceID = Message.byteArrayToLong(bytes, 7, 4);
+        raceStatus = Message.byteArrayToInt(bytes, 11, 1);
+        expectedStartTime = Message.byteArrayToLong(bytes, 12, 6);
+        windDirection = Message.byteArrayToInt(bytes, 18, 2) * 360 / 65536.0;
+        windSpeed = Message.byteArrayToInt(bytes, 20, 2);
+        numBoatsInRace = Message.byteArrayToInt(bytes, 22, 1);
+        raceType = Message.byteArrayToInt(bytes, 23, 1);
+
+        int indent = 24; //25th byte
+
+        boatDetailsList = new BoatStatusMessage[numBoatsInRace];
+
+        for (int i = 0; i < numBoatsInRace; i++) {
+            boatSourceID = Message.byteArrayToLong(bytes, indent, 4);
+            boatStatus = Message.byteArrayToInt(bytes, indent + 4, 1);
+            boatLegNumber = Message.byteArrayToInt(bytes, indent + 5, 1);
+            boatTimeToNextMark = Message.byteArrayToLong(bytes, indent + 8, 6);
+            boatTimeToFinish = Message.byteArrayToLong(bytes, indent + 14, 6);
+
+            BoatStatusMessage boatDetails = new BoatStatusMessage(boatSourceID, boatStatus,
+                    boatLegNumber, boatTimeToNextMark, boatTimeToFinish);
+            boatDetailsList[i] = boatDetails;
+
+            indent += 20;
+        }
+    }
+}
