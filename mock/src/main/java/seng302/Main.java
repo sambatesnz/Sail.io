97773cc46c0bf7db83bc617d495b4bc7da51a62b
Stefan Hall <sha162@uclive.ac.nz>
2017-07-26 18:02:23 +1200

    Chose a library for generating xml Implemented a generator which takes a race and generates xml based on the properties
    Signed-off-by: Sam <sba136@uclive.ac.nz>
    
    #story[1024]
    #pair[sha162, sba136]

--- mock/src/main/java/seng302/Main.java
+++ mock/src/main/java/seng302/Main.java
@@ -1,5 +1,6 @@
 package seng302;
 
+import seng302.DataGeneration.MockRace;
 import seng302.Server.Server;
 
 import java.io.IOException;
@@ -8,8 +9,10 @@ import java.io.IOException;
  * Runs the Mock server
  */
 public class Main {
-
+    private Race race = new Race();
     public static void main(String[] args) throws IOException {
+
         Server server = new Server(4941);
+
     }
-}   
+}
