eb15ef34ccbf9a3eeacbb771c58f8e8b6bb80ebe
Sam <sba136@uclive.ac.nz>
2017-07-26 19:46:41 +1200

    RaceXML generates start time 30 seconds in the future
    Signed-off-by: Sam <sba136@uclive.ac.nz>
    
    #story[1024]

--- mock/src/main/java/seng302/XMLCreation/RaceXMLCreator.java
+++ mock/src/main/java/seng302/XMLCreation/RaceXMLCreator.java
@@ -8,23 +8,19 @@ import seng302.RaceObjects.Boat;
 import seng302.RaceObjects.CompoundMark;
 import seng302.RaceObjects.CourseLimit;
 import seng302.RaceObjects.Mark;
-
-import java.io.FileWriter;
 import java.io.IOException;
-import java.time.Instant;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
+;
 
 
 public class RaceXMLCreator {
 
     Race race;
     Document raceXML;
-
-    public RaceXMLCreator(Race race){
-        this.race = race;
-        this.raceXML = DocumentHelper.createDocument();
-
-    }
 
     public Document createDocument() throws IOException {
 
@@ -33,9 +29,8 @@ public class RaceXMLCreator {
         root.addElement("CreationTimeDate").addText("/TODO");
 
 
-
         root.addElement("RaceStartTime")
-                .addAttribute("Start", "2015-08-29T13:10:00+02:00") //TODO Generate time properly
+                .addAttribute("Start", getDateString())
                 .addAttribute("Postpone", "False");
 
         root.addElement("RaceID")
@@ -46,27 +41,27 @@ public class RaceXMLCreator {
 
         Element participants = root.addElement("Participants");
 
-        for (Boat b: race.getBoats()){
+        for (Boat b : race.getBoats()) {
             participants.addElement("Yacht")
                     .addAttribute("SourceID", String.valueOf(b.getSourceId()));
         }
 
         Element course = root.addElement("Course");
 
-        for(CompoundMark mark: race.getCompoundMarks()){
+        for (CompoundMark mark : race.getCompoundMarks()) {
             Element compoundMark = course.addElement("CompoundMark")
                     .addAttribute("CompoundMarkID", String.valueOf(mark.getId()))
                     .addAttribute("Name", mark.getName());
 
             int seqid = 1;
-            for(Mark singleMark: mark.getMarks()){
+            for (Mark singleMark : mark.getMarks()) {
                 compoundMark.addElement("Mark")
                         .addAttribute("SeqID", String.valueOf(seqid))
                         .addAttribute("Name", "/TODO")
                         .addAttribute("TargetLat", String.valueOf(singleMark.getLatitude()))
                         .addAttribute("TargetLng", String.valueOf(singleMark.getLongitude()))
                         .addAttribute("SourceID", String.valueOf(singleMark.getSourceId()));
-                seqid ++;
+                seqid++;
 
             }
         }
@@ -74,56 +69,50 @@ public class RaceXMLCreator {
         Element compoundMarkSequence = root.addElement("CompoundMarkSequence");
 
         int seqId = 1;
-        for(CompoundMark mark: race.getCompoundMarks()) {
+        for (CompoundMark mark : race.getCompoundMarks()) {
             compoundMarkSequence.addElement("Corner")
                     .addAttribute("SeqID", String.valueOf(seqId))
                     .addAttribute("CompoundMarkID", String.valueOf(mark.getId()))
                     .addAttribute("Rounding", "/TODO")
                     .addAttribute("ZoneSize", "/TODO");
-            seqId ++;
+            seqId++;
         }
 
         Element courseLimit = root.addElement("CourseLimit");
 
 
         seqId = 1;
-        for(CourseLimit mark: race.getBoundaries()) {
+        for (CourseLimit mark : race.getBoundaries()) {
             courseLimit.addElement("Limit")
                     .addAttribute("SeqID", String.valueOf(seqId))
                     .addAttribute("Lat", String.valueOf(mark.getLat()))
                     .addAttribute("Lon", String.valueOf(mark.getLon()));
-            seqId ++;
+            seqId++;
         }
 
-
-
-
-
-
-
-
-
-
-//        this.raceXML.addElement("Race");
-
-        //addRoot();
-        /*Element root = document.addElement("Race");
-
-        Element author1 = root.addElement("author")
-                .addAttribute("name", "James")
-                .addAttribute("location", "UK")
-                .addText("James Strachan");
-
-        Element author2 = root.addElement("author")
-                .addAttribute("name", "Bob")
-                .addAttribute("location", "US")
-                .addText("Bob McWhirter");
-
-
-        FileWriter out = new FileWriter("foo.xml");
-        document.write(out);*/
-
         return this.raceXML;
+    }
+
+
+
+
+    private String getDateString() {
+        final long ONE_MINUTE_IN_MILLIS=60000;
+        TimeZone tz = TimeZone.getTimeZone("NZST");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX"); // Quoted "Z" to indicate UTC, no timezone offset
+        // df.setTimeZone(tz);
+        Calendar date = Calendar.getInstance();
+        long t= date.getTimeInMillis();
+        return df.format(new Date(t + ONE_MINUTE_IN_MILLIS / 2));
+    }
+
+
+
+    public RaceXMLCreator(Race race){
+        this.race = race;
+        this.raceXML = DocumentHelper.createDocument();
+
+
     }
 
 
