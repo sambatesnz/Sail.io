0afa484bf128bcc474b4f66bd0aa8f502d4383fe
Stefan Hall <sha162@uclive.ac.nz>
2017-08-23 18:35:13 +1200

    Implemented and tested a yacht event message
    #pair[sha162, jtr44] #story[1253]
    
    (cherry picked from commit 56e86ed)

---
+++ mock/src/main/java/seng302/PacketGeneration/MessageEnumUtility.java
@@ -0,0 +1,12 @@
+package seng302.PacketGeneration;
+
+/**
+ * For creating enums that store binary message index and size info
+ */
+public interface MessageEnumUtility {
+
+    public int getIndex();
+
+    public int getSize();
+
+}
