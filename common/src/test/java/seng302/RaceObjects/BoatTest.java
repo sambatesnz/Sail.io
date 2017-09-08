e99cddd5db82f6bce48ac4c342e4705a4a219ef3
Tim Garrod <tjg73@uclive.ac.nz>
2017-09-08 14:37:04 +1200

    Added unit tests for boat disconnect method
    #story[1267]

--- common/src/test/java/seng302/RaceObjects/BoatTest.java
+++ common/src/test/java/seng302/RaceObjects/BoatTest.java
@@ -1,15 +1,22 @@
 package seng302.RaceObjects;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 
 /**
  * Created by msi52 on 26/07/17.
  */
 public class BoatTest {
 
-    private Boat boat = new Boat("Test Boat", "TestyMcTestFace", 125, "Great Britain");
+    private Boat boat;
+    @Before
+    public void setup() {
+        boat = new Boat("Test Boat", "TestyMcTestFace", 125, "Great Britain");
+    }
 
     @Test
     public void testUpwindHeading() {
@@ -25,6 +32,32 @@ public class BoatTest {
         boat.updateHeading(100, false);
 
         assertEquals(153.0, boat.getHeading());
+    }
+
+    @Test
+    public void testDisconnectSailsOut() {
+        boat.setSailsOut(true);
+
+        assertTrue(boat.isSailsOut());
+        assertTrue(boat.isConnected());
+
+        boat.disconnect();
+
+        assertFalse(boat.isSailsOut());
+        assertFalse(boat.isConnected());
+    }
+
+    @Test
+    public void testDisconnectSailsIn() {
+        boat.setSailsOut(false);
+
+        assertFalse(boat.isSailsOut());
+        assertTrue(boat.isConnected());
+
+        boat.disconnect();
+
+        assertFalse(boat.isSailsOut());
+        assertFalse(boat.isConnected());
     }
 
 
