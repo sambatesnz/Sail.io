6398c0c9b95d0a4b515553fda312ad39f8485c05
Ollie Robb <osr13@uclive.ac.nz>
2017-09-09 14:26:36 +1200

    - Refactored Server to take in the cli arguments - Added documentation for some public methods
    Signed-off-by: Ollie Robb <osr13@uclive.ac.nz>
    #pair[sha162, osr13]
    #story[1268]

--- server/src/main/java/seng302/Main.java
+++ server/src/main/java/seng302/Main.java
@@ -1,11 +1,6 @@
 package seng302;
 
-import seng302.DataGeneration.IServerData;
-import seng302.DataGeneration.RaceManager;
 import seng302.Server.Server;
-
-import java.util.Arrays;
-
 /**
  * Runs the Mock server
  */
