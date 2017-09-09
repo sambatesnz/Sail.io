afef9ee4fa33bd8875dc46fb859ca7f468127a4d
Stefan Hall <sha162@uclive.ac.nz>
2017-09-09 14:54:13 +1200

    Added cucumber tests, couldnt write any as we cant get it to recognise steps
    Signed-off-by: Ollie Robb <osr13@uclive.ac.nz>
    #pair[sha162, osr13]
    #story[1268]

---
+++ server/src/test/java/seng302/steps/CliOptionsSteps.java
@@ -0,0 +1,30 @@
+package seng302.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * CLI option steps
+ */
+public class CliOptionsSteps {
+    @Given("^I want to start the race in \"([^\"]*)\" mode$")
+    public void i_want_to_start_the_race_in_mode(String arg1) throws Throwable {
+        System.out.println("here..............");
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^The server starts$")
+    public void the_server_starts() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^It should be running in \"([^\"]*)\"$")
+    public void it_should_be_running_in(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
