645542dddf427f819ef8be2712fe73394f611f4d
Matt <msi52@uclive.ac.nz>
2017-09-19 10:51:01 +1200

    Changed race start time so you only have to wait for 10 seconds on dev

--- visualiser/src/main/java/seng302/Controllers/LobbyController.java
+++ visualiser/src/main/java/seng302/Controllers/LobbyController.java
@@ -56,7 +56,7 @@ public class LobbyController {
         timeToStart.textProperty().bind(race.timeToStartProperty());
 
         timeToStart.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.equals(String.format(" %02d:%02d:%02d", 0, 1, 0)) || (race.getClientSourceId() == 0 && race.isRaceReady() && !raceStarted)) {
+            if (newValue.equals(String.format(" %02d:%02d:%02d", 0, 0, 10)) || (race.getClientSourceId() == 0 && race.isRaceReady() && !raceStarted)) {
                 Platform.runLater(
                     () -> {
                         try {
